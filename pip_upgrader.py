6abb23ddc839ee2f48a9678b1b4dec38f2ac7c58
miscy210 <miscy210@163.com>
2018-01-06 20:42:40 +0800

    Create pip_upgrader.py
    This scripts is to upgrade the all packages installed in the python environment.

---
+++ pip_upgrader.py
@@ -0,0 +1,20 @@
+import os
+import platform
+
+def get_packagelist():
+    result = os.popen("pip list")
+    if platform.system() == "Windows":
+        plst = result.split('\r\n')
+    elif platform.system() == "Linux":
+        plst = result.split('\n')
+    else:
+        plst = result.split('\r')
+    packageslist = [item.split(' ')[0] for item in plst]
+    return packageslist
+
+def pipupgrade(pl):
+    for p in pl:
+        os.system("pip install --upgrade %s"%p)
+
+if __name__ == "__main__":
+    pipupgrade(get_packagelist())
